f949e52a6c2d3090867ccdf004a013937fbbee6c
Blake <tk0wo3@gmail.com>
2023-02-12 12:33:29 +0800

    add: Selection Sort Core Code

--- SelectionSort/main.go
+++ SelectionSort/main.go
@@ -1,1 +1,15 @@
 package SelectionSort
+
+func Run(data []int) []int {
+	length := len(data)
+	for i := 0; i < length; i++ {
+		min := i
+		for j := i + 1; j < length; j++ {
+			if data[j] < data[min] {
+				min = j
+			}
+		}
+		data[i], data[min] = data[min], data[i]
+	}
+	return data
+}
