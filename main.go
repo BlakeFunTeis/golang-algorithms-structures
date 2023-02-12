4a8dfebfe5fe14b2cefc63b3762462468e966d2d
Blake <tk0wo3@gmail.com>
2023-02-12 12:33:50 +0800

    add: use Selection Sort

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"golang-algorithms-structures/BinarySearch"
+	"golang-algorithms-structures/SelectionSort"
 )
 
 func main() {
-	BinarySearchData := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	result, err := BinarySearch.Run(BinarySearchData, 99)
-	fmt.Println(result, err)
+	binarySearchData := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	binarySearchResult, err := BinarySearch.Run(binarySearchData, 99)
+	fmt.Println(binarySearchResult, err)
+	selectionSortData := []int{555, 447, 223, 11, 5, 6, 7, 8, 9, 10}
+	selectionSortResult := SelectionSort.Run(selectionSortData)
+	fmt.Println(selectionSortResult)
 }
